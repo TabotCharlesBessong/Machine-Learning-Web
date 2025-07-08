26f6df143270510b5daa5719abb0d38bc189a3bc
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-08 05:02:07 +0100

    detailed object tracking

--- football_analysis/main.py
+++ football_analysis/main.py
@@ -1,12 +1,22 @@
 
 from utils import read_video,save_video
+from trackers import Tracker
 
 def main():
   # read video
   video_frames = read_video("./input_data/08fd33_4.mp4")
   
+  # Initialize tracker
+  tracker = Tracker('./models/best.pt')
+  
+  tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path="./stubs/track_stubs.pkl")
+  
+  # draw output
+  # draw object tracks
+  output_video_frames = tracker.draw_annotations(video_frames,tracks)
+  
   # save video
-  save_video(video_frames,"./output_videos/output_video.avi")
+  save_video(output_video_frames,"./output_videos/output_video.avi")
   
 if __name__ == '__main__':
   main()
