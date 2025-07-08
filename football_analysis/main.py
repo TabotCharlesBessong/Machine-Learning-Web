570d9fb96065dcd0c2c0296e0d84e460ed8761de
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-08 02:15:39 +0100

    getting frames from videos

--- football_analysis/main.py
+++ football_analysis/main.py
@@ -1,1 +1,12 @@
-print("hello motto")
+
+from utils import read_video,save_video
+
+def main():
+  # read video
+  video_frames = read_video("./input_data/08fd33_4.mp4")
+  
+  # save video
+  save_video(video_frames,"./output_videos/output_video.avi")
+  
+if __name__ == '__main__':
+  main()
