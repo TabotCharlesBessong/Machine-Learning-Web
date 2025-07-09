fe24f0c6ad70a57b46cd7d185d85581fa58b0663
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-09 14:08:36 +0100

    camera movement estimator

--- football_analysis/main.py
+++ football_analysis/main.py
@@ -5,6 +5,7 @@ import cv2
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 import numpy as np
+from camera_movement_estimator import CameraMovementEstimator
 
 def main():
   # read video
@@ -14,6 +15,11 @@ def main():
   tracker = Tracker('./models/best.pt')
   
   tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path="./stubs/track_stubs.pkl")
+  
+  # camera movement
+  camera_movement_estimator = CameraMovementEstimator(video_frames[0])
+  camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,read_from_stub=True,stub_path="./stubs/camera_movement_stub.pkl")
+  camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
   
   # Interpolate ball position
   tracks['ball'] = tracker.interpolate_ball_positions(tracks['ball'])
@@ -48,6 +54,9 @@ def main():
   # draw output
   # draw object tracks
   output_video_frames = tracker.draw_annotations(video_frames,tracks,team_ball_control)
+  
+  # draw the camera movement
+  output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
   
   # save video
   save_video(output_video_frames,"./output_videos/output_video.avi")
