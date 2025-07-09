15b97a8f210a671de2c91bbcae0317d11d03ffef
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-09 03:41:51 +0100

    player color assignment

--- football_analysis/main.py
+++ football_analysis/main.py
@@ -1,6 +1,7 @@
 
 from utils import read_video,save_video
 from trackers import Tracker
+import cv2
 
 def main():
   # read video
@@ -11,12 +12,13 @@ def main():
   
   tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path="./stubs/track_stubs.pkl")
   
+  
   # draw output
   # draw object tracks
-  output_video_frames = tracker.draw_annotations(video_frames,tracks)
+  # output_video_frames = tracker.draw_annotations(video_frames,tracks)
   
   # save video
-  save_video(output_video_frames,"./output_videos/output_video.avi")
+  # save_video(output_video_frames,"./output_videos/output_video.avi")
   
 if __name__ == '__main__':
   main()
