e8f8690a2f160571c002cdc4f74ad7fbcd56e55e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-22 19:01:08 +0100

    player team assignment

--- nba_analysis/main.py
+++ nba_analysis/main.py
@@ -2,6 +2,7 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
 from drawers import PlayerTracksDrawer, BallTracksDrawer
+from team_assigner import TeamAssigner
 
 def main():
   # read a video file
@@ -17,6 +18,14 @@ def main():
   
   # remove bad ball detections
   player_tracks = ball_tracker.remove_wrong_detections(player_tracks)
+  # interpolate ball positions
+  ball_tracks = ball_tracker.interpolate_ball_positions(ball_tracks)
+  
+  # assign teams to players
+  team_assigner = TeamAssigner()
+  player_teams = team_assigner.get_player_teams_across_frames(video_frames,player_tracks,read_from_stub=True,stub_path="stubs/player_team_stubs.pkl")
+  
+  print("Player Teams Assigned:", player_teams)
 
 
   # draw output
@@ -24,7 +33,7 @@ def main():
   player_tracks_drawer = PlayerTracksDrawer() 
   ball_tracker_drawer = BallTracksDrawer()
   # drawe object tracks on video frames
-  output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
+  output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks,player_teams)
   output_video_frames = ball_tracker_drawer.draw(output_video_frames, ball_tracks)
   # save video frame
   save_video(output_video_frames,"output_videos/track/output_video_1.avi")
