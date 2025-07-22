0bb7c7dd968eef5d948cadd384e43fe01fcf9151
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-22 05:25:37 +0100

    tracking and prediction videos from the trained models

--- nba_analysis/main.py
+++ nba_analysis/main.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8x")  # Load a pretrained YOLOv8 model
+model = YOLO("models/court/best.pt")  # Load a pretrained YOLOv8 model
 
 # Perform inference on an image
-results = model.predict("input_videos/video_3.mp4", save=True)
+results = model.track("input_videos/video_1.mp4", save=True)
 print(results)  # Print the results of the inference
 print("======================")
 for box in results[0].boxes:
